76191d7f595f2f25635e0eec818b2fd45b89688f
wogara <weogara@gmail.com>
2024-03-19 11:32:29 -0400

    adding quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz.jsx";
 
 function App() {
 
 
-    return <Header />
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+        
+    )
 }
 
 export default App;
